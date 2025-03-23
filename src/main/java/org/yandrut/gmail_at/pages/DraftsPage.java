607c6e1d33d4ea9e29820ac6c6b46452f2fc7a05
Yandrut <nickolayko@yahoo.com>
2025-03-23 17:54:05 +0200

    add logger
    Signed-off-by: Yandrut <nickolayko@yahoo.com>

--- src/main/java/org/yandrut/gmail_at/pages/DraftsPage.java
+++ src/main/java/org/yandrut/gmail_at/pages/DraftsPage.java
@@ -1,8 +1,6 @@
 package org.yandrut.gmail_at.pages;
 
 import java.util.List;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -22,7 +20,6 @@ public class DraftsPage extends AbstractPage {
     private List<WebElement> drafts;
 
     private static final String BLANK_LOCATOR_FOR_TEXT = "//*[@class='bog']/*[contains(text(), '%s')]";
-    private static final Logger log = LogManager.getLogger(DraftsPage.class);
 
     public DraftsPage(WebDriver driver) {
         super(driver);
