f0991e1d80e6e98140a4126c0de20ab58857e098
Yandrut <nickolayko@yahoo.com>
2025-03-31 19:49:06 +0300

    add browser configuration
    Signed-off-by: Yandrut <nickolayko@yahoo.com>

--- src/main/java/org/yandrut/gmail_at/model/User.java
+++ src/main/java/org/yandrut/gmail_at/model/User.java
@@ -2,4 +2,11 @@ package org.yandrut.gmail_at.model;
 
 public record User(String email, String password) {
 
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
